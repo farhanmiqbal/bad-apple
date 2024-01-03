4f27ab781c874d50d6d0f48fe7fa7a56b05e9861
farhanmiqbal <farhanmiqbal10@gmail.com>
2024-01-03 20:51:26 +0700

    add game logic

--- tebak-angka/script.js
+++ tebak-angka/script.js
@@ -9,6 +9,10 @@
 // document.querySelector('.guess').value = 15;
 // console.log(document.querySelector('.guess').value);
 
+const secretNumber = Math.trunc(Math.random()*20)+1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess);
@@ -16,6 +20,16 @@ document.querySelector('.check').addEventListener('click', function() {
     if (!guess) {
         document.querySelector('.message').textContent = "angka tidak ditemukan"
 
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = "selamat anda benar 🎉"
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = "angka yang anda masukan terlalu besar"
+        score --;
+        document.querySelector('.score').textContent = score;
+    } else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = "angka yang anda masukan terlalu kecil"
+        score --;
+        document.querySelector('.score').textContent = score;
     }
 
 });
