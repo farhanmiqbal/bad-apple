7de178260bc7ce888ef847981376d9aa8c305378
farhanmiqbal <farhanmiqbal10@gmail.com>
2024-01-04 20:32:07 +0700

    setting a highscore

--- tebak-angka/script.js
+++ tebak-angka/script.js
@@ -11,7 +11,7 @@
 
 let secretNumber = Math.trunc(Math.random()*20)+1;
 let score = 20;
-
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -29,6 +29,10 @@ document.querySelector('.check').addEventListener('click', function() {
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
         
+        if(score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore ;
+        }
     
     // ketika salah menebak jawaban
     } else if (guess > secretNumber) {
